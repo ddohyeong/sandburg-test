ee1fdee8790715c536dabdd03afcbb17764e2182
ddohyeong <dohyeong7026@gmail.com>
2024-08-08 04:29:17 +0900

    Fix: 회원 유효성 검증 수정

--- src/users/dto/create.dto.ts
+++ src/users/dto/create.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, Matches, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({ description: 'loginId' })
     @IsNotEmpty()
     @MinLength(8)
+    @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/,{
+        message: '로그인 아이디는 최소 8자리의 영어와 숫자 조합이어야 합니다.'
+    })
     loginId: string;
 
     @ApiProperty({ description: 'password' })
     @IsNotEmpty()
     @MinLength(8)
+    @Matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/, {
+        message: '비밀번호는 최소 8자리의 영어, 숫자, 특수문자 조합이어야 합니다.',
+    })
     password: string;
 
     @ApiProperty({ description: 'email' })
