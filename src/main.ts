6238a073eebf88a685d9b3d943b00cf4bd0fbbff
ddohyeong <dohyeong7026@gmail.com>
2024-08-08 03:29:43 +0900

    Fix: swagger 수정

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './utils/swagger';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
@@ -9,18 +10,26 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Cats example')
+  //   .setDescription('The cats API description')
+  //   .setVersion('1.0')
+  //   .addTag('cats')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: '*', // 모든 도메인에서의 요청 허용 (실제 운영 환경에서는 주의 필요)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  
   const port = process.env.PORT || 8000;
   app.useGlobalPipes(new ValidationPipe());
 
+  setupSwagger(app);
   await app.listen(port);
   console.log(`listening on port ${port}`);
 }
